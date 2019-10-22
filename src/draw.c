069e2b1704cc45a2f55e3db489f91ea8a02dad8b
Fagner Luan <50180391+Fagner-Luan@users.noreply.github.com>
2019-10-22 11:18:09 -0300

    Ajustes

--- src/draw.c
+++ src/draw.c
@@ -1,6 +1,6 @@
 /*Arquivo com as implementações das funções de desenho*/
-#include "../draw.h"
-#include "../structures.h"
+#include "../inc/draw.h"
+#include "../inc/structures.h"
 
 void cleanImage() {
     
