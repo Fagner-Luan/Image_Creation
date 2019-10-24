1c2634a6d1bb7b0479f9acfe30b940a2f4180c02
Fagner Luan <50180391+Fagner-Luan@users.noreply.github.com>
2019-10-24 07:58:16 -0300

    Adição da main

--- src/main.c
+++ src/main.c
@@ -1,1 +1,40 @@
+/*Arquivo principal do programa*/
+#include "../inc/filemanip.h"
 
+int main(int argc, char *argv[])
+{
+	int userChoice;
+	Bool keepRunning = True;
+	
+	int aruments = argc;
+	
+	do {
+		
+		if (arguments == 2) {
+			strcpy(comandFileName, argv[1]);
+		}
+		else {
+			printf("Digite o nome do arquivo de especificações: ");
+			scanf("%s", comandFileName);
+		}
+		
+		arguments = 0;
+		
+		readComandFile(comandFileName);
+		
+		destroyFile(comandFile);
+		destroyFile(imageFile);
+		destroyFile(newImageFile);
+		destroyImage(image, height);
+		
+		printf("Digite 1 para trabalhar outro arquivo ou 2 para encerrar: ");
+		scanf("%d", &userChoice);
+		
+		if (userChoice == 2) {
+			keepRunning = False;
+		}
+		
+	}while (keepRunning);
+	
+	return 0;
+}
