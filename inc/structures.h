24f49ed8e474275675645fd8b9988c046b59239d
Fagner Luan <50180391+Fagner-Luan@users.noreply.github.com>
2019-10-26 07:42:39 -0300

    Pequenas alterações

--- inc/structures.h
+++ inc/structures.h
@@ -5,9 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-#define MAX_TAM 100
-
 //Tipo pixel para as imagens
 typedef struct
 {
@@ -26,51 +23,17 @@ typedef struct
     int y;
 }Point;
 
-//variavel para armazenar o numero de pontos 
-static int numberOfPoints;
-
-//vetor de pontos
-static Point points[MAX_TAM];
-
-//Estruturas necessárias para o projeto
+/*Estruturas necessárias para o projeto*/
 
 //variavel para a imagem
 static Pixel **image;
 
-//variavel para o arquivo de especificações
-static FILE *comandFile = NULL;
-
-//variavel para o arquivo da imagem
-static FILE *imageFile = NULL;
-static FILE *newImgFile = NULL;
-
-//variavel para armazenar a cor escolhida
-static Pixel color;
-
-//variavel para armazenar a cor da primitiva clear
-static Pixel clearColor;
-
-//variaveis para largura e altura da imagem
-static int width;
-static int height;
-
-//variaveis para os nomes dos arquivos
-static char comandFileName[61];
-static char imageOpenName[61];
-static char imageSaveName[61];
-
-//variavel para nome da primitiva
-static char comand[31];
+//altura e largura da imagem
+static int width, height;
 
 //variaveis para caminhos dos arquivos
 static char txtPath[] = "../TXT/";
 
 static char ppmPath[] = "../ppm/";
-
-//variavel para o ponto da primitiva fill ou outro ponto necessario
-static Point point;
-
-//variavel para armazenar a qualidade da imagem
-static int quality;
 
 #endif
